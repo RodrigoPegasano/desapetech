b4119d788fbf92f496738eee0d738ff2f6936817
Eduardo Santana <edusf0@hotmail.com>
2021-01-19 15:30:47 -0300

    atualizações

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET home page. */
+router.get('/produto', function(req, res, next) {
+  res.render('produto', { title: 'Produto' });
+});
+
 module.exports = router;
