b6f1077073204b6408e11ce4f97ba2f0fe9e51a9
Eduardo Santana <edusf0@hotmail.com>
2021-01-20 20:36:45 -0300

    criando pagina de login

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,14 @@ router.get('/', function(req, res, next) {
 });
 
 
-/* GET home page. */
+/* Rota Produto */
 router.get('/produto', function(req, res, next) {
   res.render('produto', { title: 'Produto' });
+});
+
+/* Rota Login */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
