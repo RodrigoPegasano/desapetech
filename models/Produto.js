4aeb4368c1aaa136ae41b5af3aaba1e84a240d53
Eduardo Santana <edusf0@hotmail.com>
2021-02-28 17:14:28 -0300

    adicionado rota e model produto

---
+++ models/Produto.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize/types");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    const produto = sequelize.define('produto', {
+        idProduto: {
+            type: DataType.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        nomeProduto:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        precoProduto:{
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        quantidadeProduto:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        imagemProduto:{
+            type: DataTypes.STRING
+        },
+        descricaoProduto:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        produtoAtivo:{
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        timestamps: false,
+        tableName: "usuarios"
+    });
+    return Usuario;
+}
