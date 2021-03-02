a20d3e6e63a349f18d088f88602fb2fb25cbdba8
Eduardo Santana <edusf0@hotmail.com>
2021-03-02 08:26:55 -0300

    adicionado models cartoes

---
+++ models/cartoes.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const cartoes = sequelize.define('cartoes', {
+        id_cartao: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },bandeira_cartao:{
+            type: DataTypes.STRING
+        },numero_cartao:{
+            type: DataTypes.INTEGER
+        },nome_cartao:{
+            type: DataTypes.STRING
+        },data_validade_cartao:{
+            type: DataTypes.DATE
+        },cod_cartao:{
+            type: DataTypes.TINYINT
+        },data_inclusao_cartao:{
+            type: DataTypes.DATE
+        },status_cartao:{
+            type: DataTypes.INTEGER
+        },
+        timestamps: false,
+        tableName: "cartoess"
+    });
+    return cartoes;
+}
