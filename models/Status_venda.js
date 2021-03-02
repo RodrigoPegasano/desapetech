5b3395289b387f127b8b726a6074ccdd325cd00b
Eduardo Santana <edusf0@hotmail.com>
2021-03-02 18:32:34 -0300

    adicionado migrations status_venda

---
+++ models/Status_venda.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+    const Status_venda = sequelize.define('status_venda', {
+        id_status_venda: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        data_envio:{
+            type: DataTypes.DATE            
+        },
+        data_status:{
+            type: DataTypes.DATE            
+        },
+        numero_envio:{
+            type: DataTypes.INTEGER
+        },
+        tipo_envio:{
+            type: DataTypes.STRING
+        },
+        custo_envio:{
+            type: DataTypes.INTEGER
+        },
+        num_regiao_envio:{
+            type: DataTypes.INTEGER
+        },
+        timestamps: false,
+        tableName: "status_venda"
+    });
+
+    return Status_venda;
+}
